5c3a033cb22dc5455ba8337e9cd0230e5c6d9c2a
Dragomir Enachi <dragomir.enachi.uk@gmail.com>
2025-03-31 16:44:42 +0000

    feat: student files

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,13 +1,17 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class StudentTile extends LightningElement {
-    @api student = {
-        Name: 'Dragomir Enachi',
-        PhotoUrl: '/services/images/photo/003B0FakePictId'
-    };
-    @api isSelected = false;
+	@api student = {
+		Name: "Dragomir Enachi",
+		PhotoUrl: "/services/images/photo/003B0FakePictId"
+	};
+	@api isSelected = false;
 
-    get tileSelected() {
-        return this.isSelected ? 'tile selected' : 'tile';
-    }
+	get tileSelected() {
+		return this.isSelected ? "tile selected" : "tile";
+	}
+
+	studentClick() {
+		alert(this.student.Name);
+	}
 }
